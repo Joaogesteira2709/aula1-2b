c390392fa9d190cfff4015af6d7902495b26584f
EA - João Pedro Gomes Augusto Gesteira <joao_gesteira@educadventista.org>
2026-04-13 16:33:54 -0300

    click funcional

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let titulo = document.querySelector("h1")
+let botao = document.querySelector("button")
 
 function alterarH1(){
-
+    titulo.innerText = "H1 alterado por João gesteira"
 }
+
+botao.addEventListener("click", alterarH1)
